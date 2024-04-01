9bbc823f331b183cd22952a8cd5cdd02ca6971ca
Aisyah Fitri <aisyahafitriani05@gmail.com>
2024-04-01 14:30:44 +0700

    Add TextField Controller

--- lib/widget/kontak_form.dart
+++ lib/widget/kontak_form.dart
@@ -8,48 +8,53 @@ class FormKontak extends StatefulWidget {
 }
 
 class _FormKontakState extends State<FormKontak> {
+  final _formKey = GlobalKey<FormState>();
+  final _namaController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _alamatController = TextEditingController();
+  final _notelepon = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Form(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Nama", hintText: "Masukkan Nama"
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: "Nama", hintText: "Masukkan Nama"),
+                  controller: _namaController,
                 ),
               ),
-            ),
-
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Email", hintText: "Masukkan Email"
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: "Email", hintText: "Masukkan Email"),
+                  controller: _emailController,
                 ),
               ),
-            ),
-
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Alamat", hintText: "Masukkan Alamat"
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: "Alamat", hintText: "Masukkan Alamat"),
+                  controller: _alamatController,
                 ),
               ),
-            ),
-
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "No Telepon", hintText: "Masukkan No Telepon"
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: "No Telepon", hintText: "Masukkan No Telepon"),
+                  controller: _notelepon,
                 ),
               ),
-            ),
-          ],),
-      ));
+            ],
+          ),
+        ));
   }
 }
